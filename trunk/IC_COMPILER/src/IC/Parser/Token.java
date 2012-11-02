1a6091fbd698f2720c0ae0b3c5c453ede8c68cc7
ehudt71@gmail.com <ehudt71@gmail.com@a1a92f4f-823b-e03f-a622-6ad4aebd3fda>
2012-11-02 23:41:43 +0000

    Added the field value to Token, and created the contructors

--- trunk/IC_COMPILER/src/IC/Parser/Token.java
+++ trunk/IC_COMPILER/src/IC/Parser/Token.java
@@ -3,8 +3,27 @@ package IC.Parser;
 import java_cup.runtime.Symbol;
 
 public class Token extends Symbol {
-    public Token(int id, int line) {
+	public int line;
+	public String value;
+	
+	public Token(int id, int line){
+		this(id, line, "");
+	}
+	
+    public Token(int id, int line, String value) {
         super(id, null);
+        this.line = line;
+    	this.value = value;
+    }
+    
+    public int getId() {
+    	return this.sym;
+    }
+    public String getValue() {
+    	return this.value;
+    }
+    public int getLine() {
+    	return this.line;
     }
 }
 
