d78efc3d241dfd3a12715b122b0ab74dc3dfb36b
ehudt71@gmail.com <ehudt71@gmail.com@a1a92f4f-823b-e03f-a622-6ad4aebd3fda>
2012-11-07 12:29:37 +0000

    Added javadoc

--- IC_COMPILER/src/IC/Parser/LexicalError.java
+++ IC_COMPILER/src/IC/Parser/LexicalError.java
@@ -1,27 +1,45 @@
 package IC.Parser;
 
+
+/** 
+ * Report a lexical error during lexical analysis of a source file.
+ * The exception can inlude the value of the erroneous token, the
+ * line in which it appeared and a custom error message.
+ */
 public class LexicalError extends Exception
 {
-    /**
-	 * 
-	 */
 	private int line;
 	private String value;
 	
+  /**
+   * Initialize a LexicalError object with only a custom message
+   * @param message   Error message string
+   */
 	public LexicalError(String message) {
 		this("",-1,message);
 	}
-
+  /**
+   * Initialize a LexicalError object with a token value, a line number
+   * and a custom message.
+   * @param value     Value of token that caused an error
+   * @param line      Line # where the error occured
+   * @param message   Error message string
+   */
 	public LexicalError(String value, int line, String message) {
 		super(message);
 		this.line = line + 1;
 		this.value = value;		
     }
-	
+	/** 
+   * Get the line number where the lexical analyzer encountered an error 
+   */
 	public int getLine(){
 		return line;
 	}
-	
+
+	/** 
+   * Get the token that caused the lexical error 
+   */
 	public String getValue(){
 		return value;
 	}
