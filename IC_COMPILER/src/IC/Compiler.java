9ae7e7cc9988eb9f387ef1f6dbda082260f84728
ehudt71@gmail.com <ehudt71@gmail.com@a1a92f4f-823b-e03f-a622-6ad4aebd3fda>
2012-11-09 20:41:58 +0000

    Fixed doc format

--- IC_COMPILER/src/IC/Compiler.java
+++ IC_COMPILER/src/IC/Compiler.java
@@ -7,63 +7,48 @@ import IC.Parser.Lexer;
 import IC.Parser.LexicalError;
 
 /**
- * Class Compiler is the class that goes over the text file, and run the lexer on it.
- * 
+ * Class Compiler is the class that goes over the text file, 
+ * and runs the lexer on it.
  */
 
 public class Compiler
 {
    /**
-    * 
-    * @param args is the path to the text file
-    * @throws Exception Lexical or other
+    * @param args should contain the path to the text file
     */
 	public static void main(String[] args) throws Exception
     {
-    	/** 
-    	 * Define a variable that will hold the current token 
-    	 */
-    	
+	
+    	// cuurToken will hold the current token from the scanner 
     	IC.Parser.Token currToken;
     	try {
-    		/**
-    		 *  Create the FileReader 
-    		 */
     		FileReader txtFile = new FileReader(args[0]);
     		
-    		/** 
-    		 * Create the Scanner 
-    		 */
     		Lexer scanner = new Lexer(txtFile);
     	
-    		/** Run the NextToken function on the text file untill reaching the EOF Token. 
-    		 * Print for each token the line it's written at, the Token name and if there's a value,
-    		 * the value
-    		 */
+    		/* Run the NextToken function on the text file untill 
+    		 * reaching the EOF Token. Print for each token the 
+    		 * line it's written at, the Token name and its value
+    		 * if there is one.
+       		 */
     		
     		do{
     			currToken = scanner.next_token();
     			System.out.print(currToken.getLine()+": "+ currToken.getName());
-    			if (currToken.getValue() != "")
+    			if (!currToken.getValue().isEmpty())
     				System.out.println("(" + currToken.getValue() + ")");
-    			
     			else{ 
     				System.out.println("");
     			}
     		}
     		while (currToken.getId() != IC.Parser.sym.EOF);
     	}
-    	
-    	
-    	/**
-    	 * Catch lexical Errors and print the line and the value of the token
-    	 */
+    	//Catch lexical Errors and print the line and the value of the token
     	catch (LexicalError e) {
 			System.out.println(e.getLine() + ": Lexical error: illegal token '" + e.getValue() + "'");
 		}
-    	
     	//TODO define what to do here
-    	catch (Exception e) {
+    	catch (IOException e) {
 			throw e;
 		}
     	
