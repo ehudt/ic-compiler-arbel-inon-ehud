937c6277065f44f7cc2cb1335753651a67ca6081
zinger.arbel@gmail.com <zinger.arbel@gmail.com@a1a92f4f-823b-e03f-a622-6ad4aebd3fda>
2012-11-30 12:53:17 +0000

    Wrote the syntexError class

--- trunk/IC_COMPILER/src/IC/Parser/SyntaxError.java
+++ trunk/IC_COMPILER/src/IC/Parser/SyntaxError.java
@@ -1,4 +1,17 @@
 package IC.Parser;
 
 public class SyntaxError extends Exception {
+	private int line;
+	private String token;
+	
+	public SyntaxError(int line, String token){
+		super();
+		this.line = line;
+		this.token = token;
+	}
+	
+	public String toString(){
+		return "SyntexError in line: " + line + "in Token: " + token;
+	}
+	
 }
