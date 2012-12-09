4bac02b83f7d556a87c05dd34610618dcea893f1
zinger.arbel@gmail.com <zinger.arbel@gmail.com@a1a92f4f-823b-e03f-a622-6ad4aebd3fda>
2012-12-09 14:44:53 +0000

    documented

--- IC_COMPILER/src/IC/Parser/SyntaxError.java
+++ IC_COMPILER/src/IC/Parser/SyntaxError.java
@@ -1,25 +1,33 @@
 package IC.Parser;
 
-import com.sun.java_cup.internal.runtime.Symbol;
-
 public class SyntaxError extends Exception {
 	/**
-	 * 
-	 */
+	 * Report a syntactic error during syntactic analysis of a source file.	 
+	 * */
+	
 	private static final long serialVersionUID = 1L;
-	private int line;
-	private String token;
+	//private int line;
+	//private String token;
+	
+	/**
+	 * Initialize a SyntaxError object with the line number and a custom message
+	 * @param line 		Line # where the error occurred
+	 * @param message   Error message string
+	 **/
 	
 	public SyntaxError(int line, String message){
 		super(line + ": Syntax error: " + message);
-		this.line = line;
-		this.token = token;
+		//this.line = line;
+		//this.token = token;
 	}
 	
+	/**
+	 * Initialize a SyntaxError object with the line number, the token name and the
+	 * token value (if token has value)
+	 * @param token		The token that caused the error
+	 **/
 	public SyntaxError(Token token){
 		super(token.getLine() + ": Syntax error in token: " + Token.getTokenName(token.getId()) + 
 				(token.value != "" ? " (" + token.value + ")" : ""));
 	}
-
-	
 }
